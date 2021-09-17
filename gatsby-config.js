683321c70adac84d421d2edd8300ba23c89cb166
Isuru Dharmadasa <isurud.14@cse.mrt.ac.lk>
2021-09-17 18:08:35 +0530

    Change root URL page

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
         navigation: [
           {
             title: `Home`,
-            slug: `/home`,
+            slug: `/`,
           },
           {
             title: `Gallery`,
